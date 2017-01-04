e2a0e6b1d0b47aa0cbac738c455dd4f5d8159f6d
lee <liwei19880505@126.com>
2017-01-04 17:06:06 +0800

    ci

---
+++ VCtrans(news)/新闻/XWCurrentViewController.m
@@ -0,0 +1,37 @@
+//
+//  XWCurrentViewController.m
+//  新闻
+//
+//  Created by lw on 15/9/16.
+//  Copyright © 2015年 lw. All rights reserved.
+//
+
+#import "XWCurrentViewController.h"
+
+@interface XWCurrentViewController ()
+
+@end
+
+@implementation XWCurrentViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
