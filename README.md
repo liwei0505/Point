7d18312c93ec763b4bf3bd1fe9db607a5982d2ad
lee <liwei19880505@126.com>
2017-02-28 16:05:07 +0800

    动态方法解析实现@dynamic

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 #####block<br/>
 #####ClassCluster(类族)
 父类根据type统一管理子类，子类不暴露，接口`简单`
+#####
 ```oc
     insert code
 ```
--- demo/demo/ViewController.m
+++ demo/demo/ViewController.m
@@ -8,6 +8,7 @@
 
 #import "ViewController.h"
 #import "EOCEmployee.h"
+#import "EOCAutoDictionary.h"
 
 @interface ViewController ()
 
@@ -28,5 +29,15 @@
     [developer doADaysWork];
 }
 
+//动态方法解析
+//其他属性相似，要添加新属性，用@property定义，声明为@dynamic即可
+//CoreAnimation框架中，CALayer就用了类似实现方式，使得CALayer成为“兼容于键值编码的”容器类，能随意添加属性，键值对形式访问
+//于是，开发者可以向其中新增自定义属性，这些值的存储工作由基类直接负责，只要在CALayer子类中定义新属性即可
+- (void)autoSelector {
+    EOCAutoDictionary *dict = [EOCAutoDictionary new];
+    dict.date = [NSDate dateWithTimeIntervalSince1970:475372800];
+    NSLog(@"date = %@",dict.date);
+    
+}
 
 @end
