2f2bfc68f1436fb43278ee6eb03fdbba5ac68f95
lee <liwei19880505@126.com>
2017-03-01 17:01:25 +0800

    运行时方法交换

--- demo/demo/ViewController.m
+++ demo/demo/ViewController.m
@@ -7,8 +7,16 @@
 //
 
 #import "ViewController.h"
+#import <objc/runtime.h>
+#import "NSString+EOCMyAdditions.h"
 #import "EOCEmployee.h"
 #import "EOCAutoDictionary.h"
+
+typedef NS_ENUM(NSInteger, demoList) {
+    DEMO_CLUSTER,//类族
+    DEMO_AUTOSELECTOR,//动态方法解析
+    DEMO_METHODSWIZZLING,//运行时方法交换
+};
 
 @interface ViewController ()
 
@@ -18,7 +26,31 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    NSInteger type = DEMO_METHODSWIZZLING;
+    switch (type) {
+        case DEMO_CLUSTER:
+            [self clusterDemo];
+            break;
+        case DEMO_AUTOSELECTOR:
+            [self autoSelector];
+            break;
+        case DEMO_METHODSWIZZLING:
+            [self methodSwizzling];
+            break;
+        default:
+            break;
+    }
     
+}
+
+- (void)methodSwizzling {
+    //方法交换一般只用来调试，项目中应用会导致代码不易懂
+    Method original = class_getInstanceMethod([NSString class], @selector(lowercaseString));
+    Method swapped = class_getInstanceMethod([NSString class], @selector(eoc_myLowercaseString));
+    method_exchangeImplementations(original, swapped);
+    NSString *str = @"ThIs iS tHe StRiNg";
+    NSString *lower = [str lowercaseString];
+    NSLog(@"%@",lower);
 }
 
 //类族模式 通过父类type创建不同子类使接口简单，但实际仍是返回的子类，并且执行的也是子类的方法
@@ -34,6 +66,7 @@
 //CoreAnimation框架中，CALayer就用了类似实现方式，使得CALayer成为“兼容于键值编码的”容器类，能随意添加属性，键值对形式访问
 //于是，开发者可以向其中新增自定义属性，这些值的存储工作由基类直接负责，只要在CALayer子类中定义新属性即可
 - (void)autoSelector {
+    //以setter方法为例，@dynamic不实现setter，动态解析来处理setter，getter方法
     EOCAutoDictionary *dict = [EOCAutoDictionary new];
     dict.date = [NSDate dateWithTimeIntervalSince1970:475372800];
     NSLog(@"date = %@",dict.date);
